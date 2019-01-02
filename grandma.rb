0eadf250039e7c7f20794bde1fd9515258a18e42
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-02 17:44:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,19 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(phrase)
+  
+  upphrase = phrase.upcase
+  
+  if phrase == upphrase
+    
+    return ""
+    
+  else
+    
+    return 
+  
+end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
