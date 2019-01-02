6e3babd2c024874310e361aa248b3f5e576dc400
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-02 17:47:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,9 +8,11 @@ def speak_to_grandma(phrase)
     
     if phrase == "I LOVE YOU GRANDMA!"
       
-      return 
+      return "I LOVE YOU TOO PUMPKIN!"
     
-    return "NO, NOT SINCE 1938!"
+    else 
+      
+      return "NO, NOT SINCE 1938!"
     
   else
     
